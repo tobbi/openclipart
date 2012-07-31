5ed6790afe073c02abeff492bffc3103d09b641e
Jakub Jankiewicz <jcubic@onet.pl>
2012-07-31 12:14:33 +0200

    move file at the begining and use aiki_users when insering

--- migration.sql
+++ migration.sql
@@ -12,16 +12,16 @@ DROP TABLE IF EXISTS openclipart_groups;
 DROP TABLE IF EXISTS openclipart_user_groups;
 DROP TABLE IF EXISTS openclipart_file_usage;
 
+-- FILES
+CREATE TABLE openclipart_files(id integer NOT NULL auto_increment, filename varchar(255), title varchar(255), seo_name varchar(255), owner integer NOT NULL, sha1 varchar(40), downloads integer, hidden boolean default 0, created datetime, modifed datetime, PRIMARY KEY(id), FOREIGN KEY(owner) REFERENCES openclipart_users(id));
+
+INSERT INTO openclipart_files(id, filename, title, owner, sha1, downloads, hidden, nsfw, created) SELECT ocal_files.id, filename, upload_name, userid, sha1, file_num_download, not upload_published, upload_date FROM ocal_files INNER JOIN aiki_users ON aiki_users.username = ocal_files.user_name;
+
 -- USERS
 CREATE TABLE openclipart_users(id integer NOT NULL auto_increment, user_name varchar(255), password varchar(60), full_name varchar(255), country varchar(255), email varchar(255), avatar integer, homepage varchar(255), user_group integer, creation_date datetime, notify boolean, nsfw_filter boolean, rand_key varchar(40), PRIMARY KEY(id), FOREIGN KEY(user_group) REFERENCES openclipart_users_groups(id), FOREIGN KEY(avatar) REFERENCES openclipart_files(id));
 
 -- copy non duplicate aiki_users
 INSERT INTO openclipart_users(id, user_name, password, full_name, country, email, avatar, homepage, user_group, creation_date, notify, nsfw_filter) SELECT minids.userid, username, password, full_name, country, email, case RIGHT(avatar, 3) when 'svg' then (select openclipart_files.id from openclipart_files where filename = users.avatar AND users.userid = owner) else null end as avatar, homepage, usergroup, first_login, notify, nsfwfilter FROM aiki_users users INNER JOIN (SELECT MIN(userid) as userid FROM aiki_users GROUP by username) minids ON minids.userid = users.userid;
-
--- FILES [RERUN]
-CREATE TABLE openclipart_files(id integer NOT NULL auto_increment, filename varchar(255), title varchar(255), seo_name varchar(255), owner integer NOT NULL, sha1 varchar(40), downloads integer, hidden boolean default 0, created datetime, modifed datetime, PRIMARY KEY(id), FOREIGN KEY(owner) REFERENCES openclipart_users(id));
-
-INSERT INTO openclipart_files(id, filename, title, owner, sha1, downloads, hidden, nsfw, created) SELECT ocal_files.id, filename, upload_name, openclipart_users.id, sha1, file_num_download, not upload_published, upload_date FROM ocal_files INNER JOIN openclipart_users ON openclipart_users.user_name = ocal_files.user_name;
 
 
 -- REMIXES
@@ -66,6 +66,6 @@ INSERT INTO openclipart_groups VALUES(1, 'Admin'), (2, 'Librarian');
 CREATE TABLE openclipart_user_groups(user_group integer NOT NULL, user integer NOT NULL, PRIMARY KEY(user_group, user), FOREIGN KEY(user_group) REFERENCES openclipart_groups(id), FOREIGN KEY(user) REFERENCES openclipart_users(id));
 
 
--- SHOWCASE
+-- CLIPART in USE [NEW]
 
 CREATE TABLE openclipart_file_usage(id integer NOT NULL auto_increment, filename VARCHAR(255), clipart integer NOT NULL, primary key(id), FOREIGN KEY(clipart) REFERENCES openclipart_files(id));
