3ed5cd26b56cef2e987355d904467ccc7288c738
Jakub Jankiewicz <jcubic@onet.pl>
2012-08-15 00:00:18 +0200

    Add AGPL License

--- libs/ArrayObjectFacade.php
+++ libs/ArrayObjectFacade.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ *  This file is part of Open Clipart Library <http://openclipart.org>
+ *
+ *  Open Clipart Library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Open Clipart Library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Open Clipart Library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  author: Jakub Jankiewicz <http://jcubic.pl>
+ */
+
 
 class ArrayObjectFacade {
     protected $data;
--- libs/Template.php
+++ libs/Template.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ *  This file is part of Open Clipart Library <http://openclipart.org>
+ *
+ *  Open Clipart Library is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Open Clipart Library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Open Clipart Library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  author: Jakub Jankiewicz <http://jcubic.pl>
+ */
 
 require_once('mustache.php/src/Mustache/Autoloader.php');
 Mustache_Autoloader::register();
@@ -17,9 +36,6 @@ class Template {
     function __construct($name, $data_privider) {
         global $indent;
         $this->name = $name;
-        if (TEMPLATE_DEBUG) {
-            echo str_repeat(' ', $indent) . 'new ' . $this->name . "\n";
-        }
         $filename = "templates/${name}.template";
         if (!file_exists($filename)) {
             throw new TemplateException("file '$filename' not found");
@@ -55,8 +71,8 @@ class Template {
                 }
             }
             if (TEMPLATE_DEBUG) {
-                echo str_repeat('  ', $indent) . $this->name . " " . gettype($ret) .
-                    '[' . count($ret) . "]\n";
+                echo str_repeat('  ', $indent) . $this->name . " " .
+                    gettype($ret) . '[' . count($ret) . "]\n";
             }
             $data = array();
             foreach ($user_data as $name => $value) {
